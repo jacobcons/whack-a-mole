416650ceda9a8f0feb12ab38f6ac5bdbccbbe5d0
Jacob <jacobcons@gmail.com>
2024-08-11 14:26:58 +0000

    setup cd to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/whack-a-mole/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
